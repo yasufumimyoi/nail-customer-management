b0787a7cc233f2f9a6f66d8ab7ac94efa5b4b733
yasufumimyoi <yasufumi.myoi@gmail.com>
2024-12-22 19:00:07 +0900

    feat:設定ページのUI追加

--- app/routes/settings/route.tsx
+++ app/routes/settings/route.tsx
@@ -1,20 +1,52 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 export const meta: MetaFunction = () => {
-  return [{ title: "Clients" }];
-};
-
-export const loader = async () => {
-  const result = Array.from({ length: 6 }).map(() => ({
-    url: "https://placehold.jp/400x400.png",
-  }));
-  return result;
+  return [{ title: "Settings" }];
 };
 
 export default function Index() {
   return (
     <div>
-      <div>test</div>
+      <h1 className="font-bold text-2xl">設定</h1>
+      <ul className="mt-5">
+        <li>
+          <Card>
+            <CardHeader>
+              <div className="flex justify-between items-center">
+                <div>
+                  <CardTitle>売上目標</CardTitle>
+                  <CardDescription className="mt-1">
+                    月々の目標売上などの設定する
+                  </CardDescription>
+                </div>
+                <SquareArrowOutUpRight className="h-5 w-5 text-gray-500" />
+              </div>
+            </CardHeader>
+          </Card>
+        </li>
+        <li className="mt-3">
+          <Card>
+            <CardHeader>
+              <div className="flex justify-between items-center">
+                <div>
+                  <CardTitle>施術メニュー</CardTitle>
+                  <CardDescription className="mt-1">
+                    提供しているメニューについて設定する
+                  </CardDescription>
+                </div>
+                <SquareArrowOutUpRight className="h-5 w-5 text-gray-500" />
+              </div>
+            </CardHeader>
+          </Card>
+        </li>
+      </ul>
     </div>
   );
 }
